bb7e7b2ab02ce3f041eb1067c2dd4367211cc199
Christoffer Andersen <kraken027@yahoo.com>
2025-05-27 12:23:42 -0400

    Update may27_2.py
    finished algorithm

--- medium/may27_2.py
+++ medium/may27_2.py
@@ -39,7 +39,33 @@ class ListNode:
 
 class Solution:
     def addTwoNumbers(self, l1, l2) -> ListNode:
-        pass
+        l1_current = l1
+        l2_current = l2
+
+        ans_current = ListNode()
+        ans_head = ans_current
+        carry = 0
+
+        while l1_current or l2_current or carry == 1:
+            val1 = l1_current.val if l1_current else 0
+            val2 = l2_current.val if l2_current else 0
+            ans_current.val = (val1 + val2 + carry) % 10
+            if val1 + val2 + carry > 9:
+                carry = 1
+            else:
+                carry = 0
+
+            if l1_current:
+                l1_current = l1_current.next
+            if l2_current:
+                l2_current = l2_current.next
+            
+            if l1_current or l2_current or carry:
+                ans_next = ListNode()
+                ans_current.next = ans_next
+                ans_current = ans_current.next
+
+        return ans_head
 
 
 # helper functions to transform lists to linked lists and back.
@@ -75,14 +101,18 @@ def print_linked_list(head):
         current = current.next
 
 
-"""
+
 def main():
-    l1 = create_linked_list([2,4,3])
-    l2 = create_linked_list([5,6,4])
+    l1 = create_linked_list([9,9,9,9,9,9,9])
+    l2 = create_linked_list([9,9,9,9])
+    
 
+    s = Solution()
+    sum = s.addTwoNumbers(l1, l2)
+    print_linked_list(sum)
 
-    s = Solution
-    s.addTwoNumbers
+main()
+
 """
 
 l1 = [2,4,3,6,8,4,3]
@@ -92,3 +122,4 @@ print_linked_list(head)
 
 node_list = linked_list_to_list(head)
 print(node_list)
+"""
